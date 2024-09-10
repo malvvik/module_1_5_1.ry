1848d39c0c64b738fa84b2715b1057101d5231aa
malvvik <malvvik@gmail.com>
2024-09-10 10:15:13 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 2, 'a','b')
+print ("immutable_var:", immutable_var )
+# immutable_var[0] = 200
+mutable_list = [[1, 2], 'a', 'b', 'Modified']
+mutable_list [0][1] = 'c'
+print('mutable_list:', mutable_list)
